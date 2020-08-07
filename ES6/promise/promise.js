c159a04b4eabd2bcde93d70f7f04bfacb2557422
774812825@qq.com <774812825@qq.com>
2020-08-07 22:48:04 +0800

    update promise self

--- ES6/promise/promise.js
+++ ES6/promise/promise.js
@@ -1,5 +1,5 @@
 ;(function () {
-  function Promise(excutor) {
+  function PromiseSelf(excutor) {
     const self = this
     self.state = 'pending'
     self.data = null
@@ -43,7 +43,7 @@
   //有可能是pending状态
   // 也有可能是resolved状态
   // 或者rejected
-  Promise.prototype.then = function (onresolved, onrejected) {
+  PromiseSelf.prototype.then = function (onresolved, onrejected) {
     const self = this
     // if (self.state === 'pengding') {
     //   self.callbacks.push({ onresolved, onrejected })
@@ -76,7 +76,49 @@
       }
     })
   }
-  Promise.race = function () {}
-  Promise.all = function () {}
-  window.Promise = Promise
+  PromiseSelf.race = function (promises) {
+    if (!Array.isArray(promises)) {
+      throw 'arguments must be Array'
+    }
+
+    return new Promise((resolve, reject) => {
+      promises.forEach((o) => {
+        Promise.resolve(o).then(
+          (value) => {
+            resolve(value)
+          },
+          (reason) => {
+            reject(reason)
+          }
+        )
+      })
+    })
+  }
+  PromiseSelf.all = function (promises) {
+    if (!promises instanceof Array) {
+      throw 'arguments must be array'
+    }
+    let values = [],
+      len = promises.length,
+      count = 0
+    return new Promise((resolve, reject) => {
+      function process(i, data) {
+        values[i] = data
+        if (++count === len) {
+          resolve(values)
+        }
+      }
+      promises.forEach((o, index) => {
+        Promise.resolve(o).then(
+          (value) => {
+            process(index, value)
+          },
+          (reason) => {
+            reject(reason)
+          }
+        )
+      })
+    })
+  }
+  window.PromiseSelf = PromiseSelf
 })()
